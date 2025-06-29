4be1d41918599ba0e82d836e1924eff356ee5ef2
Yousef Shaheen <yousef97.sh.ys@gmail.com>
2025-06-30 01:15:49 +0200

    Update os_memory.h

--- os_memory.h
+++ os_memory.h
@@ -41,6 +41,32 @@ AllocStrategy os_getAllocationStrategy(Heap const* heap);
 void os_freeProcessMemory(Heap* heap, ProcessID pid);
 MemAddr os_realloc(Heap* heap, MemAddr addr, uint16_t size);
 
+/////////////////////////////////////////////////////////
+
+#define MEMORY_FREE                0x0
+#define MEMORY_FOLLOWS             0xF
+
+
+#define MEMORY_SHARED_CLOSED           0x8
+#define MEMORY_SHARED_FOLLOWS          0x9
+#define MEMORY_SHARED_READ_1         0xA
+#define MEMORY_SHARED_READ_2       0xB
+#define MEMORY_SHARED_WRITE            0xC
+
+////////////////////////////////////////////////////////////
+
+// Shared memory management
+MemAddr os_sh_malloc(Heap* heap, uint16_t size);
+void os_sh_free(Heap* heap, MemAddr* ptr);
+
+MemAddr os_sh_readOpen(Heap const* heap, MemAddr const* ptr);
+MemAddr os_sh_writeOpen(Heap const* heap, MemAddr const* ptr);
+void os_sh_close(Heap const* heap, MemAddr addr);
+
+void os_sh_write(Heap const* heap, MemAddr const* ptr, uint16_t offset, MemValue const* dataSrc, uint16_t length);
+
+void os_sh_read(Heap const* heap, MemAddr const* ptr, uint16_t offset, MemValue* dataDest, uint16_t length);
+
 
 
 
